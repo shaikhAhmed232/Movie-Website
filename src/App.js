9d9fbd9442f1646c994b36912dcb7dc138fb7a1c
ShaikhAhmed232 <stitans270@gmail.com>
2022-02-10 16:18:04 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import requests from "./request";
 import Header from "./components/Header";
 import Movies from "./components/main_page/Movies";
+import TvSeries from './components/main_page/TvSeries'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
         <Header />
         <Routes>
         <Route path="/" element={<Movies fetchingUrl={requests.fetchMovies} />} />
+        <Route path="tvseries" element={<TvSeries />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
